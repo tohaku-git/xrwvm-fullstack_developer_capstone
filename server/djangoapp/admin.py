dad0e0ce78db4b026e92387e828a29837f5d9749
Your Name <you@example.com>
2024-07-21 05:58:59 -0400

    Module 3-2

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -7,7 +7,15 @@
 # CarModelInline class
 
 # CarModelAdmin class
+# class CarModelAdmin(models.ModelAdmin):
+#     fields = ['car_make', 'name', 'type' 'year']
 
 # CarMakeAdmin class with CarModelInline
+# class CarMakeAdmin(models.ModelAdmin):
+#     fields = ['name', 'description']
 
 # Register models here
+# admin.site.register(CarModel, CarModelAdmin)
+# admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel)
+admin.site.register(CarMake)
