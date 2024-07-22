5dd597b014f667375a8f3cd3713666c5982d8ec7
Your Name <you@example.com>
2024-07-22 10:46:07 -0400

    Module 5-1-4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -109,7 +109,7 @@ def get_dealerships(request, state="All"):
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status": 200, "dealers": dealerships})  
+    return JsonResponse({"status": 200, "dealers": dealerships})
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
