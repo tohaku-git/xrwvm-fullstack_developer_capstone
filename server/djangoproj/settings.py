cf5390c468df7ca5191fca6b522f6ecce6f373c3
Your Name <you@example.com>
2024-07-22 10:09:59 -0400

    Module 5-1-1

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,12 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://myt1907-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://myt1907-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://myt1907-8000.theianext-0-labs-'
+    'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://myt1907-8000.theiadockernext-1-labs-'
+    'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://myt1907-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://myt1907-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://myt1907-8000.theianext-0-labs-'
+    'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://myt1907-8000.theiadockernext-1-labs-'
+    'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
@@ -101,19 +105,23 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
@@ -150,4 +158,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
